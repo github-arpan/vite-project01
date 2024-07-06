d1305ad046dc0218dd75f7b29a00bacc1e312dac
github-arpan <develop.arpan@gmail.com>
2024-07-06 23:49:28 +0530

    removing some file

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-
-
-
+import { useState } from "react";
 
 function App() {
-  let [counter, setCounter] = useState(10)
+  let [counter, setCounter] = useState(10);
 
-  const addValue = () =>{
-    if(counter<20){
-      setCounter(counter+1)
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
     }
-   
-  }
-  const removeValue = () =>{
-    if(counter>0){
-      setCounter(counter-1)
+  };
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
-  }
+  };
 
   return (
-  <div>
-    <h1>counter : {counter}</h1>  
-  <button
-  onClick={addValue}
-  >plus</button>
-  <button
-  onClick={removeValue}
-  >minus</button>
-  </div>
-)
+    <div className="counterDiv">
+      <h1>counter : {counter}</h1>
+      <button onClick={addValue}>plus</button>
+      <button onClick={removeValue}>minus</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
